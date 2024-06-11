defb62983568f4c0cfa0672eef96ad01ede1f1e4
Arpita <157292031+codingM123@users.noreply.github.com>
2024-06-11 16:33:38 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // output 6 in browser(console) not in nodejs
+
+let name = "arpita" // string data type
+let age = 20 // number data type 
+let single = true // boolean data type 
+
+
+// number => 2  to power 53
+// bigint
+//string => ""
+// boolean => true / false 
+// null => standalone value (undefined nhi h but khali h)
+//undefined => variable declared but value not assigned
+// symbol => unique
+
+// object (important)
+
+console.log(typeof age);  // tell about data type
+console.log(typeof null); // type : object 
+console.log(typeof undefined); // undefined type
