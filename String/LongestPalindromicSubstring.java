675958d710c8dbe07aeb319a6471904aadeba04e
Arpita <157292031+codingM123@users.noreply.github.com>
2025-08-12 18:28:23 +0530

    Create LongestPalindromicSubstring.java

---
+++ String/LongestPalindromicSubstring.java
@@ -0,0 +1,26 @@
+
+public class LongestPalindromicSubstring {
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0; // indices of best palindrome
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);     // odd length
+            int len2 = expandAroundCenter(s, i, i + 1); // even length
+            int len = Math.max(len1, len2);
+            if (len > end - start + 1) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        // after loop, palindrome is (left+1 .. right-1)
+        return right - left - 1;
+    }
